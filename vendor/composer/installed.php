798aebcf3ed2016167feb93f0a0c580ebd90b0dd
LokiMATI <r.matigorov@yandex.ru>
2025-11-25 19:13:51 +0300

    add autoload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '208e2413c830feae7762afec8ed474b678daf4a9',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '208e2413c830feae7762afec8ed474b678daf4a9',
+            'dev_requirement' => false,
+        ),
+    ),
+);
